60e85db76e608be253f8c35c403b3e89dfa84a53
Bisola Oladipo <bo22aag@herts.ac.uk>
2023-04-19 14:23:41 +0100

    starting files

---
+++ WFC/src/Booking.java
@@ -0,0 +1,11 @@
+public class Booking {
+    private Customer customer;
+    private Lesson lesson;
+    // other fields and methods as required
+
+    public Booking(Customer customer, Lesson lesson) {
+        this.customer = customer;
+        this.lesson = lesson;
+    }
+    // getters and setters for fields
+}
---
+++ WFC/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static final Scanner input = new Scanner(System.in);
+    Customer customer = new Customer();
+    public void main(String[]args){
+        System.out.println("Welcome to The Weekend Fitness Club!");
+        System.out.println("Please enter your phone number");
+        String answer = input.next();
+        //customer.customer1(answer);
+    }
+}
